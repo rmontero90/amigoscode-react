210cbb3089aff5095c05103d5ee9316d3b853b61
Rafael Montero <developer@rmontero.me>
2026-04-01 11:03:23 -0600

    Initial commit
    Initial for workshop

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card title="My Card" description="This is a simple card component." />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = ({ title, description }) => {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+};
+
+export default Card;
