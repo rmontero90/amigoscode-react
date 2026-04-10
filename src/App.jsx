8e725af33fd978738eb3afb14e32ec3c46f429ef
Rafael Montero <developer@rmontero.me>
2026-04-09 19:10:45 -0600

    ErrorBoundary case
    Handle errors related to rendering components. Doesn't cover async functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Card from "./components/Card";
 import BlogPost from "./components/BlogPost";
 import { videos } from "./video-data";
 import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
+
+/* ** Suspense Case ***
 const Details = lazy(async() => {
   await new Promise((res) => setTimeout(res, 2000));
   return await import('./components/Details');
@@ -15,6 +18,31 @@ const App = () => {
 return showDetails && (
     <>
     <Suspense fallback={<div>Loading...</div>} ><Details /></Suspense>
+     {videos.map(video => (
+        <Card 
+          key={video.id}
+          video={video}
+        />
+      ))} 
+    </>
+  );
+};
+
+*/
+
+
+/* ** Error Boundary Case ** */
+
+const Details = lazy(() => {
+  return Promise.reject
+});
+
+const App = () => {
+  const showDetails = true;
+
+return showDetails && (
+    <>
+    <ErrorBoundary fallback={<div>Something has gone wrong</div>} ><Details /></ErrorBoundary>
      {videos.map(video => (
         <Card 
           key={video.id}
