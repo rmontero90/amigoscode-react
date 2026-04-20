353c0c6814eb18c97ab7e5aa2f8d238b329ad09f
Rafael Montero <developer@rmontero.me>
2026-04-19 18:42:29 -0600

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import styles from './App.module.css';
-import CounterTwo from './components/CounterTwo';
-// import Counter from "./components/Counter";
+// import CounterTwo from './components/CounterTwo';
+import Counter from "./components/Counter";
 
 
 /* Exercise */
 
 const App = () => {
 
-
   return (
-    <div className={styles.wrapper}>
-      <CounterTwo />
-    </div>
+    <Counter />
   )
 
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [mounted, setMounted] = useState(false);
+    
+    useEffect(() => {
+        console.log('Counter mounted');
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (mounted) {
+            console.log('Counter value changed');
+        }
+        return () => console.log('Counter has unmounted');
+    }, [count]);
     return (
         <section>
             <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
         </section>    
     );
 };
