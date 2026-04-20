8b74e9d5358b151ae836a7b4308b2c6bc0095af9
Rafael Montero <developer@rmontero.me>
2026-04-19 22:15:43 -0600

    useReducer

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,25 +1,31 @@
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
-    const [mounted, setMounted] = useState(false);
-    
-    useEffect(() => {
-        console.log('Counter mounted');
-        setMounted(true);
-    }, []);
 
-    useEffect(() => {
-        if (mounted) {
-            console.log('Counter value changed');
-        }
-        return () => console.log('Counter has unmounted');
-    }, [count]);
+    const initialState = {
+        countOne: 0,
+        countTwo: 0
+    }
+    const counterReducer = (state, action) => {
+        const actions = {
+            increment: () => ({...state, countOne: state.countOne + 1}),
+            decrement: () => ({...state, countTwo: state.countTwo - 1}),
+            reset: () => initialState
+        };
+
+        return actions[action] ? actions[action]() : state;
+    }
+
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+    
+
     return (
         <section>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
-            <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+            <h1>{state.countOne}</h1>
+            <button onClick={() => dispatch('increment')}>+</button>
+            <h1>{state.countTwo}</h1>
+            <button onClick={() => dispatch('decrement')}>-</button>
+            <button onClick={() => dispatch('reset')}>Reset</button>
         </section>    
     );
 };
